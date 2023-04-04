9e12942db481bce33435d0623fc0ea6b0383ae74
Lukas <mrspinkyspy@gmail.com>
2023-04-04 16:38:42 +0200

    Fixed Band name generator.

--- 1 - Working With Variables/Exercise 5 - Band Name Generator.py
+++ 1 - Working With Variables/Exercise 5 - Band Name Generator.py
@@ -1,21 +1,15 @@
+# Coded by Lukasz Spychala
+
 import os
 os.system("cls")
 
-zmienna1 = "test"
-zmienna2 = "jakas dluga"
-print(f"Jakis tekst{zmienna1:->40s}")
-print(f"Jakis tekst{zmienna2:->40s}")
+print("Welcome to the Band Name Generator")
+city = input("What's name of the city you grew up in?\n")
+petName = input("What's your pet's name?\n")
+print("Your band name could be " + city + " " + petName)
 
-print("{}".format(zmienna2.center(20, "-")))
-
-
-
-
-# print("Welcome to the Band Name Generator")
-# city = input("What's name of the city you grew up in?\n")
-# petName = input("What's your pet's name?\n")
-# print("Your band name could be " + city + " " + petName)
-
-
-
-
+# zmienna1 = "test"
+# zmienna2 = "jakas dluga"
+# print(f"Jakis tekst{zmienna1:->40s}")
+# print(f"Jakis tekst{zmienna2:->40s}")
+# print("{}".format(zmienna2.center(20, "-")))
