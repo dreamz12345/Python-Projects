2bb0c4e3b9a4f0473ae15666daaceb9dd0b85c20
Lukas <mrspinkyspy@gmail.com>
2023-04-06 15:43:20 +0200

    Added Folder 6 and Exercise 1.

---
+++ 6 - Python Functions/Exercise 1 - The Hurdles Loop.py
@@ -0,0 +1,32 @@
+
+# this code is supposed to be run at reeborg.ca
+
+# These two functions exist only to stop VS Code showing errors because
+# turn_left() and move() functions are not defined
+#  |
+#  V
+def turn_left():
+    ""
+def move():
+    ""
+#  ^
+#  |
+
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def move_and_jump():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+for loop in range(0, 6):
+    move_and_jump()
