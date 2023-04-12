30e15e6c8315166a9cd009bc42f3ea439d540cff
Lukas <mrspinkyspy@gmail.com>
2023-04-12 19:10:35 +0200

    Add formating and comments.

--- 7 - Hangman/Exercise 4 - Final Project.py
+++ 7 - Hangman/Exercise 4 - Final Project.py
@@ -12,23 +12,39 @@ for _ in chosenWord:
 
 # Loop until all blanks are filled, main game loop
 while "_" in chosenWordDisplay:
+
     playerGuess = input("Guess a letter: ").lower()
+
+    # Search for player chosen letter in hidden word
+    # if letter found fill blank with letter
     for index, letter in enumerate(chosenWord):
         if playerGuess == letter:
             isPlayerGuessCorrect = True
             chosenWordDisplay[index] = playerGuess
+
+    # Reduce livesLeft if player didn't guess a letter
     if isPlayerGuessCorrect == False:
         livesLeft -= 1
     isPlayerGuessCorrect = False
+
+    # If livesLeft == 0 end the game
     if livesLeft == 0:
         os.system("cls")
-        print("".join(chosenWordDisplay))
+        # Show wordInProgress as string instead of list
+        wordInProgress = ""
+        wordInProgress = wordInProgress.join(chosenWordDisplay)
+        print(f"Word to uncover: {wordInProgress}")
         print(ASCII_Art.stages[livesLeft])
         print(f"You Lose!\nCorrect word was: {chosenWord}\n")
         exit()
-    else:
-        os.system("cls")
-        print("".join(chosenWordDisplay))
-        print(ASCII_Art.stages[livesLeft])
 
+    # Print current player standing
+    os.system("cls")
+    # Show wordInProgress as string instead of list
+    wordInProgress = ""
+    wordInProgress = wordInProgress.join(chosenWordDisplay)
+    print(f"Word to uncover: {wordInProgress}")
+    print(ASCII_Art.stages[livesLeft])
+
+# If out of while loop -> all blanks filled -> Player wins!
 print("You Win!\n")
