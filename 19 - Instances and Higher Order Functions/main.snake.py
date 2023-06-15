f3f91f090a0bf2a8df7899467133da6482fb9f8f
Lukas <mrspinkyspy@gmail.com>
2023-06-15 19:12:49 +0200

    Snake backup + gitignore

--- 19 - Instances and Higher Order Functions/main.snake.py
+++ 19 - Instances and Higher Order Functions/main.snake.py
@@ -17,11 +17,12 @@ class GameSettings:
         self.frame_delay_ms = 10
 
         self.angle = 1
-        self.angle_max = 15
-        self.angle_increment = 0.5
+        self.angle_max = 10
+        self.angle_increment = 0.1
 
         self.food_shape = "circle"
         self.food_shape_size = 1.5
+        self.food_hitbox_size = 30
         self.food_color = "red"
 
         self.snake_shape = "circle"
@@ -36,12 +37,12 @@ class SnakeGame:
 
     def __init__(self) -> None:
 
-        self.settings = GameSettings()
+        self.s = GameSettings()
         self.keys_pressed = set("u")
         self.actions = {
-            "u": lambda: self.snake.forward(self.settings.step_speed),
-            "l": lambda: self.snake.left(self.settings.angle),
-            "r": lambda: self.snake.right(self.settings.angle),
+            "u": lambda: self.snake.forward(self.s.step_speed),
+            "l": lambda: self.snake.left(self.s.angle),
+            "r": lambda: self.snake.right(self.s.angle),
         }
 
         self.food = turtle.Turtle()
@@ -50,23 +51,23 @@ class SnakeGame:
 
         self.snake = turtle.Turtle()
         self.starting_snake_settings()
+        self.snake_hitbox = []
 
         self.screen = turtle.Screen()
         self.screen.tracer(0)
-        self.screen.setup(self.settings.window_width,
-                          self.settings.window_height)
-        self.screen.title(f"{self.settings.game_title}    "
-                          f"Score: {self.snake_len}")
-        self.screen.colormode(self.settings.colormode)
+        self.screen.setup(self.s.window_width,
+                          self.s.window_height)
+        self.screen.title(self.s.game_title)
+        self.screen.colormode(self.s.colormode)
 
 
     def starting_snake_settings(self):
 
-        line_size = self.settings.snake_line_size
-        shape_size = self.settings.snake_shape_size
+        line_size = self.s.snake_line_size
+        shape_size = self.s.snake_shape_size
         self.snake_num_of_stamps = 0
         self.snake_len = 0
-        self.snake.shape(self.settings.snake_shape)
+        self.snake.shape(self.s.snake_shape)
         self.snake.pensize(line_size)
         self.snake.shapesize(shape_size, shape_size)
         self.snake.penup()
@@ -75,9 +76,9 @@ class SnakeGame:
     def starting_food_settings(self):
 
         self.food.hideturtle()
-        self.food.shape(self.settings.food_shape)
-        self.food.shapesize(self.settings.food_shape_size)
-        self.food.color(self.settings.food_color)
+        self.food.shape(self.s.food_shape)
+        self.food.shapesize(self.s.food_shape_size)
+        self.food.color(self.s.food_color)
         self.food.penup()
 
 
@@ -85,53 +86,75 @@ class SnakeGame:
 
         self.food.showturtle()
         if not self.food_exists:
-            random_x = random.randint((self.settings.window_width / -2),
-                                      (self.settings.window_width / 2))
-            random_y = random.randint((self.settings.window_height / -2),
-                                      (self.settings.window_height / 2))
+            random_x = random.randint((self.s.window_width / -2),
+                                      (self.s.window_width / 2))
+            random_y = random.randint((self.s.window_height / -2),
+                                      (self.s.window_height / 2))
             self.food.goto((random_x, random_y))
             self.food_exists = True
 
 
     def snake_keep_length(self):
 
-        if self.snake_num_of_stamps > self.snake_len:
-            self.snake_num_of_stamps -= 1
+        if len(self.snake_hitbox) > self.snake_len:
             self.snake.clearstamps(1)
+            if self.snake_hitbox is not None:
+                first_stamp = self.snake_hitbox[0]
+                self.snake_hitbox.remove(first_stamp)
 
 
-    def snake_stamp(self):
+    def make_stamp(self):
 
-        if self.settings.snake_stamp_clock < self.settings.snake_stamp_reset:
-            self.settings.snake_stamp_clock += 1
+        if self.s.snake_stamp_clock < self.s.snake_stamp_reset:
+            self.s.snake_stamp_clock += 1
         else:
-            self.snake.stamp()
-            self.snake_num_of_stamps += 1
-            self.settings.snake_stamp_clock = 0
+            if self.snake_len > 0:
+                self.snake.stamp()
+                stamp_hitbox = self.create_hitbox(self.snake, 30)
+                self.snake_hitbox.append(stamp_hitbox)
+                self.s.snake_stamp_clock = 0
+
+
+    def create_hitbox(self, game_object, hitbox_size) -> tuple:
+
+        hitbox = (round(game_object.position()[0] - hitbox_size),
+                  round(game_object.position()[0] + hitbox_size),
+                  round(game_object.position()[1] - hitbox_size),
+                  round(game_object.position()[1] + hitbox_size))
+        return hitbox
 
 
     def snake_eat(self):
 
-        #snake_position_x = self.snake.position()
-        #snake_position_y = list(self.snake.position()[1])
-        food_position_x = (self.food.position()[0] - 30, self.food.position()[0] + 30)
-        food_position_y = (self.food.position()[1] - 30, self.food.position()[1] + 30)
+        # Create hitbox around food object centre
+        FOOD_HITBOX_SIZE = self.s.food_hitbox_size
+        food_position_x = (self.food.position()[0] - FOOD_HITBOX_SIZE,
+                           self.food.position()[0] + FOOD_HITBOX_SIZE)
+        food_position_y = (self.food.position()[1] - FOOD_HITBOX_SIZE,
+                           self.food.position()[1] + FOOD_HITBOX_SIZE)
 
+        # If snake head inside food hitbox
         if self.food_exists:
             if self.snake.position()[0] > food_position_x[0]:
                 if self.snake.position()[0] < food_position_x[1]:
                     if self.snake.position()[1] > food_position_y[0]:
                         if self.snake.position()[1] < food_position_y[1]:
+                            # Snake increase length, print score, respawn food
                             self.snake_len += 1
                             print(self.snake_len)
                             self.food_exists = False
                             self.spawn_food()
+        
+    
+    def snake_self_damage(self):
 
-        #print(food_position_x)
+        SNAKE_HITBOX_SIZE = self.s.food_hitbox_size
+        
 
 
     def change_color(self):
 
+        # Generate random RGB color and assign it to snake
         red = random.randint(0, 255)
         green = random.randint(0, 255)
         blue = random.randint(0, 255)
@@ -141,23 +164,27 @@ class SnakeGame:
 
     def handle_keys_pressed(self):
 
+        # Search for keys pressed by the user and steer snake accordingly
         for action in self.keys_pressed:
             self.actions[action]()
 
 
     def handle_angle_speed(self):
 
+        # When snake is changing direction, increase angle degree of movement
+        # up to s.angle_max. When key is released, reset angle
         if "l" in self.keys_pressed or "r" in self.keys_pressed:
-            if self.settings.angle < self.settings.angle_max:
-                self.settings.angle += self.settings.angle_increment
+            if self.s.angle < self.s.angle_max:
+                self.s.angle += self.s.angle_increment
         else:
-            self.settings.angle = 1
+            self.s.angle = 1
 
 
     def clear_screen(self):
 
         self.screen.reset()
         self.starting_snake_settings()
+        self.starting_food_settings()
 
 
     def user_input(self):
@@ -180,11 +207,13 @@ class SnakeGame:
         self.handle_angle_speed()
         self.spawn_food()
         self.snake_eat()
-        self.snake_stamp()
+        self.make_stamp()
         self.snake_keep_length()
-        #print(self.food.position())
+        # for index, element in enumerate(self.snake_hitbox):
+        #     print(f"{index}. {element}")
+        # print("")
         self.screen.update()
-        self.screen.ontimer(fun=self.tick, t=self.settings.frame_delay_ms)
+        self.screen.ontimer(fun=self.tick, t=self.s.frame_delay_ms)
 
 
     def main(self):
