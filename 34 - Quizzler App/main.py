d9dc24318bd3299fce32f7d785e7e4de9e93ad04
Lukas <mrspinkyspy@gmail.com>
2023-09-11 15:19:52 +0200

    works

--- 34 - Quizzler App/main.py
+++ 34 - Quizzler App/main.py
@@ -1,10 +1,12 @@
 from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
+from html import unescape
 
 question_bank = []
 for question in question_data:
-    question_text = question["question"]
+    question_text: str = question["question"]
+    question_text = unescape(question_text)
     question_answer = question["correct_answer"]
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
